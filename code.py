110f8ba09bced34fffb5110731b59b16b38f9d86
layer8 <111331376+Layer812@users.noreply.github.com>
2022-12-25 23:26:17 +0900

    Update code.py

--- code.py
+++ code.py
@@ -7,7 +7,7 @@ import usb_midi
 import adafruit_midi
 from adafruit_midi.note_on import NoteOn
 from adafruit_midi.note_off import NoteOff
-pixel_pin = board.GP28
+pixel_pin = board.GP2
 num_pixels = 28
 
 skip_leds = int(num_pixels/4)
@@ -19,11 +19,13 @@ midi = adafruit_midi.MIDI(midi_in=usb_midi.ports[0], in_channel=(0,1,2,3))
 while True:
     msg_in = midi.receive()
     if isinstance(msg_in, NoteOn) and msg_in.velocity != 0:
-        vol=(msg_in.note-57)*3*3*3/7+25
+        vol=((msg_in.note-57)*3*3*3/7+25)%128
         ch=msg_in.channel*skip_leds
         pixels[ch:ch+brink_leds]=(vol,vol,vol)
         pixels.show()
     if isinstance(msg_in, NoteOff):
         ch=msg_in.channel*skip_leds
         pixels[ch:ch+brink_leds]=(0,0,0)
-        pixels.show()
+        pixels.show()    
+    
+
